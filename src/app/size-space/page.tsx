bc476ad2be6c233bcfd880188e2d308753c4488f
Serhiy <serhiy.mos@gmail.com>
2024-02-02 13:26:53 +0400

    Sizes with Overrides of pixel-precise values

---
+++ src/app/size-space/page.tsx
@@ -0,0 +1,11 @@
+const SizeSpacePage = () => {
+  return (
+    <main>
+      <div className="h-72 w-[250px] bg-[#0000CC] px-4 py-10">
+        <div className="mx-20 h-40 w-20 bg-red-200"></div>
+      </div>
+    </main>
+  );
+};
+
+export default SizeSpacePage;
