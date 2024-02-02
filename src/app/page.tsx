ee187af25d0513f186452ffa675c6e7a117aa893
Serhiy <serhiy.mos@gmail.com>
2024-02-02 13:20:31 +0400

    Fancy button styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,17 @@ export default function Home() {
   return (
     <main className="outline">
 
-      <div>
-        <button>I am a button</button>
-        <input placeholder="Input placeholder" />
+      <div className="">
+        <div className="m-2">
+          <button>I am a button</button>
+        </div>
+        <div className="m-2">
+          <button className="btn">I am a fancy button</button>
+        </div>
+        <div className="m-2">
+          <input placeholder="Input placeholder" />
+        </div>
       </div>
-
     </main>
   );
 }
