26207477bb0f4eb6c71b35bf3020c65ce71461f6
Serhiy <serhiy.mos@gmail.com>
2024-02-02 13:32:45 +0400

    Extending color palette

--- src/app/size-space/page.tsx
+++ src/app/size-space/page.tsx
@@ -1,7 +1,7 @@
 const SizeSpacePage = () => {
   return (
     <main>
-      <div className="h-72 w-[250px] bg-[#0000CC] px-4 py-10">
+      <div className="h-72 w-[250px] bg-blue-chill-500 px-4 py-10">
         <div className="mx-20 h-40 w-20 bg-red-200"></div>
       </div>
     </main>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,19 @@ const config: Config = {
       colors(theme) {
         return {
           ...theme.colors,
+          'blue-chill': {
+            '50': '#f2f9f9',
+            '100': '#ddeff0',
+            '200': '#bfe0e2',
+            '300': '#92cace',
+            '400': '#5faab1',
+            '500': '#438e96',
+            '600': '#3b757f',
+            '700': '#356169',
+            '800': '#325158',
+            '900': '#2d464c',
+            '950': '#1a2c32',
+          },
           primary: {
             ...theme.colors.purple,
             DEFAULT: theme.colors.purple['500'],
